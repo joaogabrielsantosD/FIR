7756c019a727f7df457f34c74204e6924f1e8da8
joaogabrieldonato <jgsd.2001@gmail.com>
2023-07-15 17:02:44 -0300

    Initial commit

---
+++ FIR.h
@@ -0,0 +1,32 @@
+#ifndef FIR_H
+#define FIR_H
+
+#define SIZE 2
+
+const float a=0.8;
+const float b=0.8;
+
+float FIR(float x);
+void desloca_vetor(float *vetorAddr, int tam, float valor);
+
+float FIR(float x)
+{
+    static float y_pass[SIZE] = {0,0}, x_pass[SIZE] = {0,0};
+    float y = (a+b)*y_pass[0] - a*b*y_pass[1] + (1-a-b + a*b)*x_pass[1];
+    desloca_vetor(y_pass, SIZE, y);
+    desloca_vetor(x_pass, SIZE, x);
+
+    return y;
+}
+
+void desloca_vetor(float *vetorAddr, int tam, float valor)
+{
+    for (int k=tam-1; k>0; k--)
+    {
+        *(vetorAddr+k) = *(vetorAddr+k-1);
+    }
+
+    *vetorAddr = valor;
+}
+
+#endif
