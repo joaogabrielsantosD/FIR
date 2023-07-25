2f69c13684b5e0998821b41521a9f2c33601a5ac
joaogabrieldonato <jgsd.2001@gmail.com>
2023-07-25 10:00:52 -0300

    Update to the order

--- FIR.h
+++ FIR.h
@@ -10,20 +10,48 @@
 /*You can change the size of the vetor if you needed*/
 #define SIZE 2
 
+
+/* Can use only in one variable. Using the second variable the value of the static float has change */
 class FIR {
     public:
     /*=======================================================================================================
         * "a" and "b" are the filter coefficients
         * The higher the value of the coefficient, the slower its update rate, but the stronger its filter.
         * Filter based on convolution between previous inputs.
+        * Can suport only for order equal 2
     ========================================================================================================*/
+        bool order(int ord);
         float filt(float a, float b, float x);
     private:
         /*=========================
             * Update the vetor
+            * Second filter
         =========================*/
         void move_vec(float *vetorAddr, int size, float value); 
+        float filtfilt(float A, float B, float x2);
+        float double_y;
+        bool flag=false;
 };
+
+bool FIR::order(int ord)
+{
+    if (ord==1)
+    {
+        return true;
+    } 
+
+    else if (ord==2)
+    {
+        flag=true;
+        return true;
+    }
+
+    else if (ord>2 || ord<1)
+    {
+        return false;
+    }
+    
+}
 
 float FIR::filt(float a, float b, float x)
 {
@@ -34,7 +62,9 @@ float FIR::filt(float a, float b, float x)
     move_vec(y_pass, SIZE, y);
     move_vec(x_pass, SIZE, x);
 
-    return y;
+    (flag) ?  double_y = filtfilt(a,b,y) : double_y = y; 
+
+    return double_y;
 };
 
 void FIR::move_vec(float *vetorAddr, int size, float value)
@@ -46,5 +76,17 @@ void FIR::move_vec(float *vetorAddr, int size, float value)
 
     *vetorAddr = value;
 };
+
+float FIR::filtfilt(float A, float B, float x2)
+{
+    static float y2_pass[SIZE] = {0,0}, x2_pass[SIZE] = {0,0};
+
+    float y2 = (A+B)*y2_pass[0] - A*B*y2_pass[1] + (1-A-B + A*B)*x2_pass[1];
+    
+    move_vec(y2_pass, SIZE, y2);
+    move_vec(x2_pass, SIZE, x2);
+
+    return y2;
+}
 
 #endif
--- test.cpp
+++ test.cpp
@@ -4,18 +4,29 @@
 using namespace std;
 
 FIR as;
+FIR bs;
 
-const float a=0.7,b=0.7;
-float Hz=1000;
+#define aa 0.6
+#define bb 0.65
+//const float a=0.7,b=0.7;
+//float Hz=1000;
+float r = 2500;
 
 int main () 
 {
+    int s = bs.order(2);
+    if (s)
+    {
+        printf("ok");
+    } else {
+        return -1;
+    }
+    
     while (1)
     {
     
-    float aspira = as.filt(a,b,Hz);
-
-    printf("%f\n", aspira);
+    float test = bs.filt(aa,bb,r);
+    printf("%d\n", (int)test);
 
     }
 }
