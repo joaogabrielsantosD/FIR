d30b9d1ecb5a1f7aa28ae4b5dc35778995920a23
joaogabrieldonato <jgsd.2001@gmail.com>
2023-08-05 17:00:10 -0300

    Change the parameters

--- FIR.h
+++ FIR.h
@@ -15,7 +15,7 @@ const float a = 0.6,
             b = 0.6;
 
 /* Can use only in one variable. Using the second variable the value of the static float has change */
-class FIR {
+class FIR  {
     public:
     /*========================================================================================================
         * "a" and "b" are the filter coefficients
@@ -30,7 +30,7 @@ class FIR {
         * Second filter
     ==========================*/
         void move_vec(float *vetorAddr, int size, float value); 
-        float filtfilt(float A, float B, float x2); // Second filter if you defined
+        float filtfilt(float x2); // Second filter if you defined
         float double_y;
         bool flag=false;
 };
@@ -49,7 +49,7 @@ float FIR::filt(float x)
     move_vec(y_pass, SIZE, y);
     move_vec(x_pass, SIZE, x);
 
-    (flag) ? double_y = filtfilt(a,b,y) : double_y = y; 
+    (flag) ? double_y = filtfilt(y) : double_y = y; 
 
     return double_y;
 };
@@ -64,11 +64,11 @@ void FIR::move_vec(float *vetorAddr, int size, float value)
     *vetorAddr = value;
 };
 
-float FIR::filtfilt(float A, float B, float x2)
+float FIR::filtfilt(float x2)
 {
     static float y2_pass[SIZE] = {0,0}, x2_pass[SIZE] = {0,0};
 
-    float y2 = (A+B)*y2_pass[0] - A*B*y2_pass[1] + (1-A-B + A*B)*x2_pass[1];
+    float y2 = (a+b)*y2_pass[0] - a*b*y2_pass[1] + (1-a-b + a*b)*x2_pass[1];
     
     move_vec(y2_pass, SIZE, y2);
     move_vec(x2_pass, SIZE, x2);
